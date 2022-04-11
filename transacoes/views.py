5dbd29e45ccf03ae2999dd5ff596ff8cb93c57a7
gamer hard99 <icaromeddev@gmail.com>
2022-04-11 13:55:49 -0300

    Implemented: html page with file input that prints the file's name and it's size(mb) in the console

--- transacoes/views.py
+++ transacoes/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def printar_console(request):
+    print(request.FILES['file_name'])
+    print(request.FILES['file_name'].size/1000000, "megabytes")
+    return render(request, 'index.html')
