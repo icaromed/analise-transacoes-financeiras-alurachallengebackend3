dfdfbe1995c112255f43d5466da8a5c44103647b
gamer hard99 <icaromeddev@gmail.com>
2022-04-12 08:49:40 -0300

    Update: Validating data and storing data into database

--- transacoes/models.py
+++ transacoes/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Controller(models.Model):
-    banco_origem = models.CharField(max_length=50)
-    agencia_origem = models.CharField(max_length=50)
-    conta_origem = models.CharField(max_length=50)
-    banco_destino = models.CharField(max_length=50)
-    agencia_destino = models.CharField(max_length=50)
-    conta_destino = models.CharField(max_length=50)
-    valor_transacao = models.CharField(max_length=50)
-    data_hora_transacao = models.CharField(max_length=50)
+    banco_origem = models.CharField(max_length=150)
+    agencia_origem = models.CharField(max_length=150)
+    conta_origem = models.CharField(max_length=150)
+    banco_destino = models.CharField(max_length=150)
+    agencia_destino = models.CharField(max_length=150)
+    conta_destino = models.CharField(max_length=150)
+    valor_transacao = models.CharField(max_length=150)
+    data_hora_transacao = models.CharField(max_length=150)
--- transacoes/views.py
+++ transacoes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import FileResponse
+from .models import Controller
 
 
 def index(request):
@@ -7,10 +8,37 @@ def index(request):
         return render(request, 'index.html')
 
     elif request.method == 'POST':
-        print(request.FILES['file_name'])
-        print(request.FILES['file_name'].size/1000000, "megabytes")
+
+        nome = request.FILES['file_name']
+        tamanho = request.FILES['file_name'].size / 1000000
+        if tamanho == 0:
+            raise ValueError("O arquivo está vazio")
+        print(nome)
+        print(tamanho, "megabytes")
+
         file_h = FileResponse(request.FILES['file_name'])
-        for file in file_h:
-            print(file.decode("utf-8"))
+        comandos = list(file_h.streaming_content)[0].decode('utf-8').split('\n')
+        comandos_efetuados = []
+        data_padrao = None
+        for pos in range(len(comandos)):
+            comando = comandos[pos].split(",")
+            if len(comando) != 8:
+                continue
+            if len(comandos_efetuados) == 0:
+                data_padrao = comando[-1][:10]
+            data = comando[-1][:10]
+            for item in comando:
+                if not item:
+                    data = ""
+
+            if data != data_padrao or comando in comandos_efetuados:
+                continue
+            transacao_valida = Controller(banco_origem=comando[0], agencia_origem=comando[1], conta_origem=comando[2],
+                                          banco_destino=comando[3], agencia_destino=comando[4],
+                                          conta_destino=comando[5], valor_transacao=comando[6],
+                                          data_hora_transacao=comando[7])
+            transacao_valida.save()
+            comandos_efetuados.append(comando)
+
         request.method = 'GET'
         return render(request, 'index.html')
