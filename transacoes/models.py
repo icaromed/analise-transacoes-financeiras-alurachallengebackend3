a075670fbaac3056376a734f8d5b4dea2054c34a
gamer hard99 <icaromeddev@gmail.com>
2022-04-13 08:07:30 -0300

    Added: register date/time of the file's upload and it's transaction's date | this table was added to the index

--- transacoes/models.py
+++ transacoes/models.py
@@ -10,3 +10,8 @@ class Controller(models.Model):
     conta_destino = models.CharField(max_length=150)
     valor_transacao = models.CharField(max_length=150)
     data_hora_transacao = models.CharField(max_length=150)
+
+
+class DataImportacoes(models.Model):
+    data_importacao = models.CharField(max_length=150)
+    data_transacao = models.CharField(max_length=150)
--- transacoes/views.py
+++ transacoes/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render
 from django.http import FileResponse
-from .models import Controller
+from .models import Controller, DataImportacoes
+from datetime import datetime
 
 
 def index(request):
+    todos_objetos = DataImportacoes.objects.all().order_by('data_transacao')
     if request.method == 'GET':
-        return render(request, 'index.html')
+        return render(request, 'index.html', {"todos_objetos": todos_objetos})
 
     elif request.method == 'POST':
 
         nome = request.FILES['file_name']
         tamanho = request.FILES['file_name'].size / 1000000
-        if tamanho == 0:
-            raise ValueError("O arquivo está vazio")
         print(nome)
         print(tamanho, "megabytes")
+        if tamanho == 0:
+            raise ValueError("O arquivo está vazio")
+        if not nome.__str__().endswith('.csv'):
+            raise ValueError("O arquivo não é do formato 'csv'")
 
         file_h = FileResponse(request.FILES['file_name'])
         comandos = list(file_h.streaming_content)[0].decode('utf-8').split('\n')
@@ -39,6 +43,9 @@ def index(request):
                                           data_hora_transacao=comando[7])
             transacao_valida.save()
             comandos_efetuados.append(comando)
+        importacao = DataImportacoes(data_transacao="/".join(data_padrao.split('-')[-1::-1]),
+                                     data_importacao=datetime.now().strftime('%d/%m/%Y - %H:%M:%S'))
+        importacao.save()
 
         request.method = 'GET'
-        return render(request, 'index.html')
+        return render(request, 'index.html', {"todos_objetos": todos_objetos})
