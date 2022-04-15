712b655c183f9d6ef538aac47db7349b31dd11ca
gamer hard99 <icaromeddev@gmail.com>
2022-04-15 11:52:57 -0300

    Update: bootstrap | incrementing the data validation and fixing bugs

--- backend_challenge/settings.py
+++ backend_challenge/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap3',
     'transacoes',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'backend_challenge.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'transacoes/templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -119,9 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- transacoes/views.py
+++ transacoes/views.py
@@ -5,47 +5,52 @@ from datetime import datetime
 
 
 def index(request):
-    todos_objetos = DataImportacoes.objects.all().order_by('data_transacao')
+    todos_objetos = DataImportacoes.objects.all().order_by('-data_transacao')
+
     if request.method == 'GET':
         return render(request, 'index.html', {"todos_objetos": todos_objetos})
 
     elif request.method == 'POST':
 
-        nome = request.FILES['file_name']
-        tamanho = request.FILES['file_name'].size / 1000000
-        print(nome)
-        print(tamanho, "megabytes")
-        if tamanho == 0:
-            raise ValueError("O arquivo está vazio")
-        if not nome.__str__().endswith('.csv'):
+        nome = request.FILES['file_name'].name
+        if not nome.endswith('.csv'):
             raise ValueError("O arquivo não é do formato 'csv'")
 
         file_h = FileResponse(request.FILES['file_name'])
         comandos = list(file_h.streaming_content)[0].decode('utf-8').split('\n')
+
+        data_padrao = comandos[0][-19:-9].strip()
+        if not data_padrao:
+            raise ValueError("O arquivo não pode estar vazio")
+
+        data_validacao = "/".join(data_padrao.split('-')[-1::-1])
+        if DataImportacoes.objects.filter(data_transacao=data_validacao).exists():
+            raise ValueError("Data de transação já utilizada")
+
         comandos_efetuados = []
-        data_padrao = None
         for pos in range(len(comandos)):
             comando = comandos[pos].split(",")
             if len(comando) != 8:
                 continue
-            if len(comandos_efetuados) == 0:
-                data_padrao = comando[-1][:10]
             data = comando[-1][:10]
+
             for item in comando:
                 if not item:
                     data = ""
-
-            if data != data_padrao or comando in comandos_efetuados:
+            if data != data_padrao:
                 continue
-            transacao_valida = Controller(banco_origem=comando[0], agencia_origem=comando[1], conta_origem=comando[2],
-                                          banco_destino=comando[3], agencia_destino=comando[4],
-                                          conta_destino=comando[5], valor_transacao=comando[6],
-                                          data_hora_transacao=comando[7])
+            if comando in comandos_efetuados:
+                continue
+
+            transacao_valida = Controller.objects.create(banco_origem=comando[0], agencia_origem=comando[1],
+                                                         conta_origem=comando[2], banco_destino=comando[3],
+                                                         agencia_destino=comando[4], conta_destino=comando[5],
+                                                         valor_transacao=comando[6], data_hora_transacao=comando[7])
             transacao_valida.save()
             comandos_efetuados.append(comando)
+
         importacao = DataImportacoes(data_transacao="/".join(data_padrao.split('-')[-1::-1]),
                                      data_importacao=datetime.now().strftime('%d/%m/%Y - %H:%M:%S'))
         importacao.save()
 
-        request.method = 'GET'
         return render(request, 'index.html', {"todos_objetos": todos_objetos})
