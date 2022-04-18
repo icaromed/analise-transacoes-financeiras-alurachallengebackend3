694176ce7324b4517892299686f00784d7a88c91
gamer hard99 <icaromeddev@gmail.com>
2022-04-18 20:12:05 -0300

    Update: register page incremented with database, now the users are being created

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 import random
 import smtplib
-
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 def cadastro(request):
     if request.method == "GET":
@@ -10,8 +11,24 @@ def cadastro(request):
     elif request.method == "POST":
         email = request.POST['email']
         username = request.POST['usuario']
-        senha = random.randint(100000, 999999)
+        senha = str(random.randint(100000, 999999))
+
+        if not username.strip():
+            messages.error(request, 'O usuário não pode ficar vazio')
+            return redirect('cadastro')
+        if not email.strip():
+            messages.error(request, 'O Email não pode ficar vazio')
+            return redirect('cadastro')
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'Este Email já foi cadastrado')
+            return redirect('cadastro')
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Este Nome de Usuário já foi cadastrado')
+            return redirect('cadastro')
+
+        user = User.objects.create_user(username, email, senha)
         eviar_email(email, senha)
+        user.save()
         return redirect('login')
 
 
